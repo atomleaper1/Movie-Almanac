2727284294948cf0d3f959037102c2fd4fce3861
Dikshit <nostalgialane414@gmail.com>
2026-09-25 23:01:29 +0545

    Restore Movie Almanac project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movie-Almanac
+I am trying to make a Movie Almanac for my personal use trying ,learning and using every concepts i am learning.
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+
+const API_KEY = "6db7e654887c6ccbf3149d34f0aadb78";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  );
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+
+  );
+  const data = await response.json();
+  return data.results;
+};
